25e9bf2aeaebeb7bf296b9997a09ec132cda5f5d
Cut0x <71967731+Cut0x@users.noreply.github.com>
2022-07-28 12:56:45 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $db_host="localhost";
 $db_user="root"; // nom d'utilisateur par défaut
 $db_password=""; // en général, en localhost, sur windows, il n'y a pas de mot de passe défini
-$db_name=""; // Ici, mettez le nom de la base de donnée que vous avez créé
+$db_name="tbl_user"; // Ici, mettez le nom de la base de donnée que vous avez créé (pour le tuto, c'est tbl_user)
 
 try { // try nous permet d'essayer la connexion à la base de donnée
 	$db=new PDO("mysql:host={$db_host};dbname={$db_name}",$db_user,$db_password); // Ici c'est une nouvelle connexion avec la base de donnée défini
